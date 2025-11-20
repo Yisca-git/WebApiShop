f7f8e6ad879d0df1e150c122a82de0a8c6163cf3
Libi675 <38328279757@mby.co.il>
2025-11-20 12:49:20 +0200

    code-review

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,49 +1,50 @@
 ﻿using Entities;
-using Microsoft.VisualBasic;
 using Repositories;
+
 namespace Services
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
         private readonly IUserPasswordService _userPasswordService;
+        
         public UserService(IUserRepository userRepository, IUserPasswordService userPasswordService)
         {
-            _userRepository= userRepository;
-            _userPasswordService= userPasswordService;
+            _userRepository = userRepository;
+            _userPasswordService = userPasswordService;
         }
+        
         public List<User> GetUsers()
         {
             return _userRepository.GetUsers();
         }
 
-        public User GetUserById(int Id)
+        public User GetUserById(int id)
         {
-            return _userRepository.GetUserById(Id);
+            return _userRepository.GetUserById(id);
         }
 
         public User AddUser(User user)
         {
-            if(_userPasswordService.CheckPassword(user.Password)<=2)
+            if(_userPasswordService.CheckPassword(user.Password) <= 2)
             {
                 return null;
             }
             return _userRepository.AddUser(user);
         }
 
-        public User LogIn(User LogInUser)
+        public User LogIn(User loginUser)
         {
-            
-            return _userRepository.LogIn(LogInUser);
+            return _userRepository.LogIn(loginUser);
         }
 
-        public bool UpdateUser(int id, User updeteUser)
+        public bool UpdateUser(int id, User updateUser)
         {
-            if (_userPasswordService.CheckPassword(updeteUser.Password) <= 2)
+            if (_userPasswordService.CheckPassword(updateUser.Password) <= 2)
             {
                 return false;
             }
-            _userRepository.UpdateUser(id, updeteUser);
+            _userRepository.UpdateUser(id, updateUser);
             return true;
         }
     }
